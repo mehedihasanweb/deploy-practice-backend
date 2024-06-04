0087a9a8008c7d2b05f1cb0d717519040437eeb0
mehedi hasan <mdmehedihasananik111@gmail.com>
2024-06-04 08:25:14 +0600

    done

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://mdmehedihasananik111:dkoVcEHTcZK4jTjd@cluster0.vggzedk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.vggzedk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
